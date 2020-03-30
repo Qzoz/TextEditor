9cd44bf389f7e9f61554b5e701ed10a3e91840ad
Qzoz <62541542+Qzoz@users.noreply.github.com>
2020-03-30 21:18:40 +0530

    Added Selection and editing options
    added selection of text
    added actions: delete, select all, cut, copy, paste, undo, redo

--- src/editor/Qz_Custom_Editor.js
+++ src/editor/Qz_Custom_Editor.js
@@ -1,5 +1,4 @@
-const { webFrame } = require('electron');
-
+const { webFrame, clipboard } = require('electron');
 
 var coordCurX = 0;
 var coordCurY = 0;
@@ -16,7 +15,38 @@ var readToggler = false;
 
 var cursorDirection = false;
 
+var shiftFlag = false;
+var selectionObj = {
+    flag: false,
+    lon: 0,
+    lat: 0,
+    rs: -1,
+    re: -1,
+    cs: -1,
+    ce: -1,
+    getLen: () => {
+        if (selectionObj.rs == selectionObj.re) {
+            selectionObj.lat = 0;
+            return selectionObj.ce - selectionObj.cs;
+        }
+        else {
+            let len = editorContent[selectionObj.rs].length - selectionObj.cs;
+            for (var i = selectionObj.rs + 1; i < selectionObj.re; i++) {
+                len += editorContent[i].length;
+            }
+            len += selectionObj.ce;
+            return len;
+        }
+    }
+}
+
+var undoStackSize = 5;
+var redoStackSize = 2;
+
+
 var editorContent = [""];
+var undoStack = [];
+var redoStack = [];
 const testString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789!@#$%^&*()_-+={}[]\\|\'\";:,<.>/?~`";
 
 window.onload = window.onresize = function () {
@@ -67,7 +97,7 @@ function focusCursor(axis, pxMove) {
             else {
                 if (pxMove == undefined) {
                     if (currCol == 0) {
-                        parent.scrollBy(-1*parent.scrollLeft, 0);
+                        parent.scrollBy(-1 * parent.scrollLeft, 0);
                     }
                     else if (parent.scrollLeft != 0) {
                         parent.scrollBy(parentLineCont.x + (currCol - tabCtr + tabCtr * tabLen) * charW - coordCurX, 0);
@@ -75,7 +105,7 @@ function focusCursor(axis, pxMove) {
                 }
                 else {
                     if (pxMove == 0) {
-                        parent.scrollBy(-1*parent.scrollLeft, 0);
+                        parent.scrollBy(-1 * parent.scrollLeft, 0);
                     }
                 }
             }
@@ -128,36 +158,75 @@ function keyListenerForEditor(e) {
     }
     else if (e.code == 'PageUp') {
         goToStartOfPage();
+        moveSelectionCursor('p-up');
     }
     else if (e.code == 'PageDown') {
         goToEndOfPage();
+        moveSelectionCursor('p-down');
     }
     else if (e.code == 'Delete') {
-        deleteCharacter();
+        updateUndoStack();
+        if (selectionObj.flag) {
+            deleteSelectedText();
+        }
+        else {
+            deleteCharacter();
+        }
     }
     else if (e.code == 'Backspace') {
-        backspacedCharacter();
+        updateUndoStack();
+        if (selectionObj.flag) {
+            deleteSelectedText();
+        }
+        else {
+            backspacedCharacter();
+        }
     }
     else if (e.code == 'Enter') {
+        updateUndoStack();
         newLineEntered();
+        moveSelectionCursor();
     }
     else if (e.code == 'End') {
-        goToEndOfLine()
+        goToEndOfLine();
+        moveSelectionCursor('right');
     }
     else if (e.code == 'Home') {
-        goToStartOfLine()
+        goToStartOfLine();
+        moveSelectionCursor('left');
     }
     else if (e.code == 'Tab') {
+        updateUndoStack();
         addCharacter('\t', tabLen);
+        moveSelectionCursor();
     }
     else if (e.code == 'Space') {
+        updateUndoStack();
         addCharacter(e.key, 1);
+        moveSelectionCursor();
+    }
+    else if (e.key == 'Shift') {
+        shiftFlag = true;
+        detectSelections();
     }
     else if (testString.includes(e.key)) {
+        updateUndoStack();
         addCharacter(e.key, 1);
+        moveSelectionCursor();
     }
     else {
         console.log('no use');
+    }
+}
+
+function keyListenerForKeyUp(e) {
+    if (e.altKey || e.ctrlKey) {
+        return;
+    }
+    e.preventDefault();
+    if (e.key == 'Shift') {
+        shiftFlag = false;
+        detectSelections();
     }
 }
 
@@ -187,10 +256,12 @@ window.addEventListener('click', function (e) {
 
 function startReadingForEditor() {
     document.addEventListener('keydown', keyListenerForEditor);
+    document.addEventListener('keyup', keyListenerForKeyUp);
 }
 
 function stopReadingForEditor() {
     document.removeEventListener('keydown', keyListenerForEditor);
+    document.removeEventListener('keyup', keyListenerForKeyUp);
 }
 
 
@@ -299,6 +370,7 @@ function moveCursorLeftOS() {
         let fromLeftPx = (currCol - tabCtr + tabCtr * tabLen) * charW;
         cursorDirection = false;
         moveCursorToX(fromLeftPx);
+        moveSelectionCursor('left');
     }
     else {
         if (currRow > 0) {
@@ -317,6 +389,7 @@ function moveCursorRightOS() {
         let fromLeftPx = (currCol - tabCtr + tabCtr * tabLen) * charW;
         cursorDirection = true;
         moveCursorToX(fromLeftPx);
+        moveSelectionCursor('right');
     }
     else {
         if (currRow < rowCount - 1) {
@@ -333,6 +406,7 @@ function moveCursorUpOS() {
         moveCursorToY(currRow * charH);
         adjustCursorHorizontallyByButton();
         changeLineNumberBoldness(currRow + 1, currRow);
+        moveSelectionCursor('up');
     }
 }
 // one step down
@@ -342,6 +416,7 @@ function moveCursorDownOS() {
         moveCursorToY(currRow * charH);
         adjustCursorHorizontallyByButton();
         changeLineNumberBoldness(currRow - 1, currRow);
+        moveSelectionCursor('down');
     }
 }
 
@@ -372,6 +447,7 @@ function adjustCursorHorizontallyByMouse(fromLeftPx) {
         currCol = currStr.length;
     }
     moveCursorToX(leftPx, true);
+    moveSelectionCursor();
 }
 
 function adjustCursorHorizontallyByButton() {
@@ -599,6 +675,9 @@ function displayReadData(data) {
     for (let i = 0; i < editorContent.length; i++) {
         currRow = i;
         tempStr = editorContent[i];
+        if (tempStr == undefined) {
+            continue;
+        }
         if (tempStr.charAt(tempStr.length - 1) == '\r') {
             editorContent[i] = tempStr.substring(0, tempStr.length - 1);
         }
@@ -610,4 +689,308 @@ function displayReadData(data) {
         }
     }
     goToEndOfLine();
+}
+
+/**
+ * UNDO & REDO STACK Operations
+ */
+
+function updateUndoStack() {
+    if (undoStack.length >= undoStackSize) {
+        undoStack.shift();
+    }
+    undoStack.push(editorContent.slice(0));
+}
+
+/**
+ * SELECTION ACTIONS
+ */
+
+function detectSelections() {
+    if (shiftFlag) {
+        selectionObj.flag = true;
+        if (selectionObj.rs == -1) {
+            selectionObj.rs = currRow;
+            selectionObj.cs = currCol;
+            selectionObj.re = currRow;
+            selectionObj.ce = currCol;
+        }
+    }
+    else {
+        if (selectionObj.rs == selectionObj.re) {
+            if (selectionObj.cs == selectionObj.ce) {
+                resetSelections();
+            }
+        }
+    }
+}
+
+function resetSelections() {
+    selectionObj.flag = false;
+    selectionObj.lon = 0;
+    selectionObj.lat = 0;
+    selectionObj.rs = -1;
+    selectionObj.re = -1;
+    selectionObj.cs = -1;
+    selectionObj.ce = -1;
+}
+
+function moveSelectionCursor(dirCur) {
+    if (!selectionObj.flag) {
+        return;
+    }
+    if (!shiftFlag) {
+        removeAllSelections();
+        resetSelections();
+        return;
+    }
+    if (dirCur == 'up' || dirCur == 'p-up') {
+        if (selectionObj.getLen() > 0 && selectionObj.lat == -1) {
+            selectionObj.re = currRow;
+            selectionObj.ce = currCol;
+        }
+        else {
+            selectionObj.rs = currRow;
+            selectionObj.cs = currCol;
+            selectionObj.lat = 1;
+        }
+    }
+    else if (dirCur == 'down' || dirCur == 'p-down') {
+        if (selectionObj.getLen() > 0 && selectionObj.lat == 1) {
+            selectionObj.rs = currRow;
+            selectionObj.cs = currCol;
+        }
+        else {
+            selectionObj.re = currRow;
+            selectionObj.ce = currCol;
+            selectionObj.lat = -1;
+        }
+    }
+    else if (dirCur == 'right') {
+        if (selectionObj.getLen() > 0 && selectionObj.lon == -1) {
+            if (selectionObj.lat == -1) {
+                selectionObj.ce = currCol;
+            }
+            else {
+                selectionObj.cs = currCol;
+            }
+        }
+        else {
+            if (selectionObj.lat == 1) {
+                selectionObj.cs = currCol;
+            }
+            else {
+                selectionObj.ce = currCol;
+            }
+            selectionObj.lon = 1;
+        }
+    }
+    else if (dirCur == 'left') {
+        if (selectionObj.getLen() > 0 && selectionObj.lon == 1) {
+            if (selectionObj.lat == 1) {
+                selectionObj.cs = currCol;
+            }
+            else {
+                selectionObj.ce = currCol;
+            }
+        }
+        else {
+            if (selectionObj.lat == -1) {
+                selectionObj.ce = currCol;
+            }
+            else {
+                selectionObj.cs = currCol;
+            }
+            selectionObj.lon = -1;
+        }
+    }
+    addSelections();
+}
+
+function addSelections() {
+    removeAllSelections();
+    if (selectionObj.rs == selectionObj.re) {
+        appendSelectionsElement(selectionObj.ce - selectionObj.cs, selectionObj.cs, selectionObj.rs);
+    }
+    else {
+        appendSelectionsElement(editorContent[selectionObj.rs].length - selectionObj.cs,
+            selectionObj.cs, selectionObj.rs);
+        for (var i = selectionObj.rs + 1; i < selectionObj.re; i++) {
+            appendSelectionsElement(editorContent[i].length, 0, i);
+        }
+        appendSelectionsElement(selectionObj.ce, 0, selectionObj.re);
+    }
+}
+
+function appendSelectionsElement(w, l, index) {
+    var elem = document.createElement('div');
+    let selStr = editorContent[index];
+    let lefty_Tabs = countTabs(selStr, 0, l);
+    let width_Tabs = countTabs(selStr, l, w + l);
+    elem.setAttribute("class", "qze_sel_txt_bl");
+    elem.style.width = charW * (w - width_Tabs + width_Tabs * tabLen) + "px";
+    elem.style.left = charW * (l - lefty_Tabs + lefty_Tabs * tabLen) + "px";
+    getCurrentRowDiv(index).appendChild(elem);
+}
+
+function removeAllSelections() {
+    for (var i = 0; i < rowCount; i++) {
+        removeSelections(i);
+    }
+}
+
+function removeSelections(index) {
+    var rowDiv = getCurrentRowDiv(index);
+    if (rowDiv.children.length > 1) {
+        rowDiv.children[1].remove();
+    }
+}
+
+/**
+ * 
+ * ACTIONS ON SELECTED TEXT
+ * 
+ */
+
+function deleteSelectedText() {
+    if (selectionObj.rs == selectionObj.re) {
+        var tempStr = editorContent[selectionObj.rs];
+        editorContent[selectionObj.rs] = tempStr.substring(0, selectionObj.cs) + 
+            tempStr.substring(selectionObj.ce, tempStr.length);
+    }
+    else {
+        var contentStack = []
+        for (var i = editorContent.length - 1; i >= selectionObj.re; i--) {
+            contentStack.push(editorContent.pop());
+        }
+        for (var i = selectionObj.rs + 1; i < selectionObj.re; i++) {
+            editorContent.pop();
+            getCurrentRowDiv(rowCount - 1).remove();
+            remLineNumber();
+        }
+        var tempStr = editorContent[selectionObj.rs];
+        editorContent[selectionObj.rs] = tempStr.substring(0, selectionObj.cs);
+        // Upper Line Above
+        
+        // Bottom Line Below
+        tempStr = contentStack.pop();
+        editorContent[selectionObj.rs] += tempStr.substring(selectionObj.ce, tempStr.length);
+        while (contentStack.length > 0) {
+            editorContent.push(contentStack.pop());
+        }
+        getCurrentRowDiv(rowCount - 1).remove();
+        remLineNumber();
+    }
+
+    // Move Cursor;
+    var tabs = countTabs(editorContent[selectionObj.rs], 0, selectionObj.cs);
+    moveCursorToY(charH * selectionObj.rs, true);
+    moveCursorToX(charW * (selectionObj.cs - tabs + tabs * tabLen), true);
+    currRow = selectionObj.rs;
+    currCol = selectionObj.cs;
+
+    removeAllSelections();
+    resetSelections();
+    updateAll();
+}
+
+function selectAllText() {
+    selectionObj.flag = true;
+    selectionObj.rs = 0;
+    selectionObj.re = rowCount-1;
+    selectionObj.cs = 0;
+    selectionObj.ce = editorContent[rowCount-1].length;
+    addSelections();
+    shiftFlag = false;
+}
+
+function cutTheSelectedText(){
+    if (selectionObj.flag) {
+        copyTheSelectedText();
+        deleteSelectedText();
+    }
+}
+
+function copyTheSelectedText() {
+    if (selectionObj.flag) {
+        if (selectionObj.rs == selectionObj.re) {
+            var tempStr = editorContent[selectionObj.rs];
+            clipboard.writeText(tempStr.substring(selectionObj.cs, selectionObj.ce));
+        }
+        else {
+            var tempStr = editorContent[selectionObj.rs];
+            var copiedStr = tempStr.substring(selectionObj.cs, tempStr.length) + '\n';
+            var slicedCnt = editorContent.slice(selectionObj.rs + 1, selectionObj.re);
+            if (slicedCnt.length > 0) {
+                copiedStr += slicedCnt.join('\n') + '\n';
+            }
+            tempStr = editorContent[selectionObj.re];
+            copiedStr += tempStr.substring(0, selectionObj.ce);
+            clipboard.writeText(copiedStr);
+        }
+    }
+}
+
+function pasteTheClipBoardText() {
+    var loadedStr = clipboard.readText();
+    if (loadedStr.length <= 0) {
+        return;
+    }
+    if (selectionObj.flag) {
+        deleteSelectedText();
+    }
+    loadedStr = loadedStr.split('\n');
+    if (loadedStr.length == 1) {
+        var tempStr = editorContent[currRow];
+        editorContent[currRow] = tempStr.substring(0, currCol) + loadedStr + 
+            tempStr.substring(currCol, tempStr.length);
+    }
+    else {
+        var tempCol = currCol;
+        var tempRow = currRow;
+        newLineEntered();
+        editorContent[currRow-1] += loadedStr[0];
+        editorContent[currRow] = loadedStr[loadedStr.length - 1] + editorContent[currRow];
+        var tempStack = []
+        for (var i = currRow; i < rowCount; i++) {
+            tempStack.push(editorContent.pop());
+        }
+        for (var i = 1; i < loadedStr.length - 1; i++) {
+            createNewLine();
+            addLineNumber();
+            editorContent.push(loadedStr[i]);
+        }
+        while (tempStack.length > 0) {
+            editorContent.push(tempStack.pop());
+        }
+        var tabs = countTabs(editorContent[tempRow], 0, tempCol);
+        moveCursorToY(charH * tempRow, true);
+        moveCursorToX(charW * (tempCol - tabs + tabs * tabLen), true);
+        currRow = tempRow;
+        currCol = tempCol;
+    }
+    updateAll();
+}
+
+function undoTheText() {
+    if (undoStack.length <= 0) {
+        return;
+    }
+    if (redoStack.length >= redoStackSize) {
+        redoStack.shift();
+    }
+    redoStack.push(editorContent.slice(0));
+    editorContent = undoStack.pop();
+    displayReadData(editorContent.join('\n'));
+    adjustCursorHorizontallyByButton();
+}
+
+function redoTheText() {
+    if (redoStack.length <= 0) {
+        return;
+    }
+    editorContent = redoStack.pop();
+    updateAll();
+    displayReadData(editorContent.join('\n'));
+    adjustCursorHorizontallyByButton();
 }
